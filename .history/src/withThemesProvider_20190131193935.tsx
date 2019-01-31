b40e54e7d53bfa951776d21a1fb5b2cea9783683
Anton Bortnikov <a.bortnikov@xsolla.com>
2019-01-31 21:39:07 +0500

    tqesqt

---
+++ .history/src/withThemesProvider_20190131193935.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { List } from "immutable";
+import { ThemesProvider } from "./ThemesProvider";
+
+import { Theme } from "./types/Theme";
+
+export const withThemesProvider = (themes: Theme[], cb: (theme: Theme) => void) => (story): JSX.Element => {
+    return <ThemesProvider themes={List(themes)} cb={cb}>{story()}</ThemesProvider>;
+};
